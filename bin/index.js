eb00c1af338aa2619e99ca358b049726e2e246d1
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-05-30 01:28:30 +0800

    shell

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+var process = require('child_process');
+process.exec('webpack-dev-server --config node_modules/ak-webpack/src/config.js --mode development',function (error, stdout, stderr) {
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
 
 console.log("hello word!")
