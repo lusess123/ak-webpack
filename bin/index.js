3821a51e329f58f8fe41496156b2670e79848ea4
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-05-30 01:35:18 +0800

    输出命令

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 var process = require('child_process');
+var iconv = require('iconv-lite');
+var encoding = 'cp936';
+var binaryEncoding = 'binary';
+
 process.exec('webpack-dev-server --config node_modules/ak-webpack/src/config.js --mode development',function (error, stdout, stderr) {
   if (error !== null) {
     console.log('exec error: ' + error);
   }
+  console.log(iconv.decode(new Buffer(stdout, binaryEncoding), encoding), iconv.decode(new Buffer(stderr, binaryEncoding), encoding));
 });
 
 console.log("hello word!")
