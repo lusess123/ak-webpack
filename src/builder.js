26a86332e965b516f6a4334bd06e6fbd852eeaee
北名 <beiming@beimingdeMacBook-Pro.local>
2020-06-14 17:15:03 +0800

    add options

--- src/builder.js
+++ src/builder.js
@@ -13,214 +13,231 @@ console.log('./：', path.resolve('./'))
 
 const publicPath = path.join(process.cwd(), '/public')
 const rootpath = path.dirname(require.main.filename)
-console.log('publicPath:',publicPath)
-console.log('rootpath:',rootpath)
+console.log('publicPath:', publicPath)
+console.log('rootpath:', rootpath)
 const processPath = process.cwd()
-console.log('processPath:',processPath)
+console.log('processPath:', processPath)
 
 const devMode = process.env.NODE_ENV !== 'production';
 console.log(process.env.NODE_ENV)
 console.log('是否只允许GET请求', process.env.ONLY_ALLOW_GET)
 
-console.log('parallel:  '+ (process.env.parallel ==='yes' ? undefined : false))
-console.log('默认启用https,如果要禁用请覆盖devServer/https:false')
-console.log('该默认证书的域名是*.terminus.io')
-console.log('默认证书的过期时间是 2029年11月12日 星期一 中国标准时间 下午2:26:52')
-console.log(`启用证书请点击${__dirname}/certificate/server.key`)
+console.log('parallel:  ' + (process.env.parallel === 'yes' ? undefined : false))
+// console.log('默认启用https,如果要禁用请覆盖devServer/https:false')
+// console.log('该默认证书的域名是*.terminus.io')
+// console.log('默认证书的过期时间是 2029年11月12日 星期一 中国标准时间 下午2:26:52')
+// console.log(`启用证书请点击${__dirname}/certificate/server.key`)
 
-console.log('mac电脑里，进入 ‘钥匙串访问’ 设置信任证书 ')
+// console.log('mac电脑里，进入 ‘钥匙串访问’ 设置信任证书 ')
 
 
 module.exports = ({ title, tsImportPluginFactoryOptions = [], alias, tsLoaderInclude = [] }) => {
   return {
-  devtool:  process.env.NODE_ENV === 'production' ? 'none':'eval-source-map',
-  stats:'minimal',
-  entry: {
-    app: './test/index.tsx',
-  },
-  optimization: {
-    minimizer: process.env.NODE_ENV === 'production' ? [new TerserPlugin({
-      parallel: process.env.parallel ==='yes' ? undefined : false,
-      cache:true,
-      sourceMap:false
-    })] : [],
-  },
-  output: {
-    path: publicPath,
-    filename: '[name].js',
-    chunkFilename: '[name].[chunkhash].js',
-    publicPath: '/',
-  },
-  devServer: {
-    contentBase: path.join(processPath, 'public'),
-    compress: true,
-    historyApiFallback: true,
-    open: true,
-    watchOptions: {
-      ignored: []
+    devtool: process.env.NODE_ENV === 'production' ? 'none' : 'eval-source-map',
+    // stats:'minimal',
+    stats: {
+      warningsFilter: /export .* was not found in/,
+      all: false,
+      colors: true,
+      depth: true,
+      entrypoints: true,
+      env: true,
+      errors: true,
+      version: true,
+      reasons: true,
+      // sedExports: true,
+      errorDetails: true,
+      timings: true,
     },
-    https: {
-      key: fs.readFileSync(path.join(__dirname,'./certificate/server.key')),
-      cert: fs.readFileSync(path.join(__dirname,'./certificate/server.crt')),
-      ca: fs.readFileSync(path.join(__dirname,'./certificate/ca.csr')),
+    entry: {
+      app: './test/index.tsx',
     },
-  },
+    optimization: {
+      minimizer: process.env.NODE_ENV === 'production' ? [new TerserPlugin({
+        parallel: process.env.parallel === 'yes' ? undefined : false,
+        cache: true,
+        sourceMap: false
+      })] : [],
+    },
+    output: {
+      path: publicPath,
+      filename: '[name].js',
+      chunkFilename: '[name].[chunkhash].js',
+      publicPath: '/',
+    },
+    devServer: {
+      contentBase: path.join(processPath, 'public'),
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      watchOptions: {
+        ignored: []
+      },
+      // https: {
+      //   key: fs.readFileSync(path.join(__dirname,'./certificate/server.key')),
+      //   cert: fs.readFileSync(path.join(__dirname,'./certificate/server.crt')),
+      //   ca: fs.readFileSync(path.join(__dirname,'./certificate/ca.csr')),
+      // },
+    },
 
-  resolve: {
-    symlinks: false,
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.less', '.json'],
-    alias
-  },
-  module:{
-    rules:[
-      {
-        test: /\.(txt|hbs)$/i,
-        use: 'raw-loader',
-      },
-      {
-        test: /\.md$/,
-        use: [
-            {
-                loader: "html-loader"
-            },
-            {
-                loader: "markdown-loader",
-                // options: {
-                //     /* your options here */
-                // }
-            }
-        ]
-      },
-      {
-        test: /requirejs\/require\.js$/,
-        use: ['script-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(tsx?|jsx?)$/,
-        loader: 'ts-loader',
-        include: [
-          ...tsLoaderInclude,
-          path.resolve(processPath,'src'),
-          path.resolve(processPath,'config'),
-          path.resolve(processPath,'test')
-        ],
-        options: {
-          allowTsInNodeModules: true,
-          transpileOnly: true,
-          happyPackMode: true,
-          getCustomTransformers: () => ({
-            before: [tsImportPluginFactory([
-              {
-                libraryName: 'antd',
-                style: true,
-              },
-              ...tsImportPluginFactoryOptions
-            ])],
-          }),
+    resolve: {
+      symlinks: false,
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.less', '.json'],
+      alias
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(txt|hbs)$/i,
+          use: 'raw-loader',
         },
-      },
-      {
-        test: /\.(css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // 'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.scss$/,
-        include: [
-          path.resolve('./node_modules'),
-        ],
-        exclude: [
-          path.resolve(processPath, 'src'),
-          path.resolve(processPath, 'test'),
-        ],
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
+        {
+          test: /\.md$/,
+          use: [
+            {
+              loader: "html-loader"
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
+            {
+              loader: "markdown-loader",
+              // options: {
+              //     /* your options here */
+              // }
+            }
+          ]
+        },
+        {
+          test: /requirejs\/require\.js$/,
+          use: ['script-loader'],
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
             },
+          ],
+        },
+        {
+          test: /\.(tsx?|jsx?)$/,
+          loader: 'ts-loader',
+          include: [
+            ...tsLoaderInclude,
+            // path.resolve(processPath, 'node_modules/@sandun'),
+            path.resolve(processPath, 'src'),
+            path.resolve(processPath, 'config'),
+            path.resolve(processPath, 'test')
+          ],
+          options: {
+            allowTsInNodeModules: true,
+            transpileOnly: true,
+            happyPackMode: true,
+            // warningsFilter: /export .* was not found in/,
+            getCustomTransformers: () => ({
+              before: [tsImportPluginFactory([
+                {
+                  libraryName: 'antd',
+                  style: true,
+                },
+                ...tsImportPluginFactoryOptions
+              ])],
+            }),
           },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        include: [
-          path.resolve(processPath, 'src'),
-          path.resolve(processPath, 'test'),
-        ],
-        exclude: [
-          path.resolve(processPath, 'node_modules'),
-        ],
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              localIdentName: '[name]_[local]-[hash:base64:5]',
+        },
+        {
+          test: /\.(css)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            // 'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          include: [
+            path.resolve('./node_modules'),
+          ],
+          exclude: [
+            path.resolve(processPath, 'src'),
+            path.resolve(processPath, 'test'),
+          ],
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          include: [
+            path.resolve(processPath, 'src'),
+            path.resolve(processPath, 'test'),
+          ],
+          exclude: [
+            path.resolve(processPath, 'node_modules'),
+          ],
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+                localIdentName: '[name]_[local]-[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
 
-      {
-        test: /\.(less)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true,
-              javascriptEnabled: true,
+        {
+          test: /\.(less)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'less-loader',
+              options: {
+                sourceMap: true,
+                javascriptEnabled: true,
+              },
             },
-          },
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new CompressionPlugin(),
-    new HtmlWebpackPlugin({
-      title,
-      chunks: ['app'],
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'index.html'),
-      hash: true,
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    }),new webpack.DefinePlugin({
-      "process.sysenv": {
-          "ONLY_ALLOW_GET":  process.env.ONLY_ALLOW_GET ? JSON.stringify(true) : JSON.stringify(false)
-      }
-    })]
-}}
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new CompressionPlugin(),
+      new HtmlWebpackPlugin({
+        title,
+        chunks: ['app'],
+        filename: 'index.html',
+        template: path.resolve(__dirname, 'index.html'),
+        hash: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      }), new webpack.DefinePlugin({
+        "process.sysenv": {
+          "ONLY_ALLOW_GET": process.env.ONLY_ALLOW_GET ? JSON.stringify(true) : JSON.stringify(false)
+        }
+      })]
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./builder')
-module.exports = (config = {}, title = 'Demo') => {
-     // var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
-     // // console.log("gggggg : "+args)
-     // var list = args.slice(0,1)
-     // // list.push(baseConfig)
-     // return merge.apply(null, [baseConfig].concat(list))
-     return merge.smart(baseConfig({title}),config)
+const path = require('path')
+const processPath = process.cwd()
+module.exports = (config = {}, title = 'Demo', options = {} ) => {
+     return merge.smart(baseConfig({
+       title,
+        ...options
+      }),config)
 }
